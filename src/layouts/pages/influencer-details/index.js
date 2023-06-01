baa437d267fa278aba27ec3c0efd779040fa863f
khrystyna <hrystynka.dm@gmail.com>
2023-06-01 13:09:58 +0300

    add posts form

--- src/layouts/pages/influencer-details/index.js
+++ src/layouts/pages/influencer-details/index.js
@@ -2,12 +2,7 @@ import { Box, Card, CircularProgress, Grid, Typography } from "@mui/material";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import SalesByCountry from "layouts/dashboards/analytics/components/SalesByCountry";
 import EngagementChart from "./EngagementChart";
-import PostsCard from "examples/Cards/PostCard";
-import booking1 from "assets/images/products/product-1-min.jpg";
 import kal from "assets/images/kal-visuals-square.jpg";
 
 import ComplexProjectCard from "examples/Cards/ProjectCards/ComplexProjectCard";
@@ -17,7 +12,6 @@ import burceMars from "assets/images/bruce-mars.jpg";
 import { useQuery } from "react-query";
 import posts from "api/posts";
 import { useParams } from "react-router-dom";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 
 // @mui icons
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -25,6 +19,23 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import Chart from "../widgets/components/Chart";
 import caloriesChartData from "../widgets/data/caloriesChartData";
+
+// Material Dashboard 2 PRO React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
+import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
+
+// Images
+import homeDecor1 from "assets/images/home-decor-1.jpg";
+import homeDecor2 from "assets/images/home-decor-2.jpg";
+import homeDecor3 from "assets/images/home-decor-3.jpg";
+import homeDecor4 from "assets/images/home-decor-4.jpeg";
+import team1 from "assets/images/team-1.jpg";
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
+import InfluencerPostsForm from "./Form";
 
 function InfluencerDetails() {
   const params = useParams();
@@ -248,6 +259,96 @@ function InfluencerDetails() {
               ))}
             </Grid>
           </Box>
+          <Box mt={6}>
+            <InfluencerPostsForm/>
+          </Box>
+          <MDBox p={2} mt={6} mb={4}>
+            <MDTypography variant="h6" mb={4}>
+              Posts
+            </MDTypography>
+            <Grid container spacing={6}>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor1}
+                  label="project #2"
+                  title="modern"
+                  description="As Uber works through a huge amount of internal management turmoil."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "dark",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor2}
+                  label="project #1"
+                  title="scandinavian"
+                  description="Music is something that everyone has their own specific opinion about."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "dark",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team4, name: "Peterson" },
+                    { image: team1, name: "Elena Morison" },
+                    { image: team2, name: "Ryan Milly" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor3}
+                  label="project #3"
+                  title="minimalist"
+                  description="Different people have different taste, and various types of music. Different ..."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "dark",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team4, name: "Peterson" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team1, name: "Elena Morison" },
+                  ]}
+                />
+              </Grid>
+              <Grid item xs={12} md={6} xl={3}>
+                <DefaultProjectCard
+                  image={homeDecor4}
+                  label="project #4"
+                  title="gothic"
+                  description="Why would anyone pick blue over pink? Pink is obviously a better color."
+                  action={{
+                    type: "internal",
+                    route: "/pages/profile/profile-overview",
+                    color: "dark",
+                    label: "view project",
+                  }}
+                  authors={[
+                    { image: team4, name: "Peterson" },
+                    { image: team3, name: "Nick Daniel" },
+                    { image: team2, name: "Ryan Milly" },
+                    { image: team1, name: "Elena Morison" },
+                  ]}
+                />
+              </Grid>
+            </Grid>
+          </MDBox>
         </Card>
       )}
     </DashboardLayout>
